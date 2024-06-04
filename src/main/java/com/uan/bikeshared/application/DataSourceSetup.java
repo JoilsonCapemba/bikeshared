c690054615e9ae3881c6b9a2d19afd3d4c3b1868
JoelsonBotelho <silvabotelho20@gmail.com>
2024-06-04 15:52:35 +0100

    Configurando a base de dados...

---
+++ src/main/java/com/uan/bikeshared/application/DataSourceSetup.java
@@ -0,0 +1,36 @@
+package com.uan.bikeshared.application;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.uan.bikeshared.repository"})
+@ComponentScan(value = "com.uan.bikeshared.*")
+@EntityScan(basePackages = {"com.uan.bikeshared.models"})
+public class DataSourceSetup {
+    @Value("${spring.datasource.url}")
+    String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    String databaseUser;
+
+    @Value("${spring.datasource.password}")
+    String databasePassword;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUser);
+        dataSource.setPassword(databasePassword);
+        return dataSource;
+    }
+}
