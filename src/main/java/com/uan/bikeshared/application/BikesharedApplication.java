3087ab76adb19b4175f05bda394811e025273b05
JoelsonBotelho <silvabotelho20@gmail.com>
2024-06-04 14:52:05 +0100

    Alterando a estrutura do projecto

--- src/main/java/com/uan/bikeshared/application/BikesharedApplication.java
+++ src/main/java/com/uan/bikeshared/application/BikesharedApplication.java
@@ -1,4 +1,4 @@
-package com.uan.bikeshared;
+package com.uan.bikeshared.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
