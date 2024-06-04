24bc3104484363baf0c9a0840cd4dc44a482f4b9
JoilsonCapemba <joilsoncapemba@gmail.com>
2024-06-04 07:39:21 -0300

    configuracao do webservice

---
+++ src/main/java/com/uan/bikeshared/BikesharedApplication.java
@@ -0,0 +1,13 @@
+package com.uan.bikeshared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikesharedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uan/bikeshared/BikesharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uan.bikeshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
