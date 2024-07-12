6ae32d849480319c407be289ad76d77f5de3f7c1
JoelsonBotelho <silvabotelho20@gmail.com>
2024-07-12 18:15:34 +0100

    Impl. os endpoints e os seguintes métodos estão disponíveis para o cliente: " + login + senPoint + verifyUser + "

--- src/main/java/com/uan/bikeshared/application/UserEndpoint.java
+++ src/main/java/com/uan/bikeshared/application/UserEndpoint.java
@@ -8,8 +8,14 @@ import com.uan.bikeshared.interfaces.GetAllUsersRequest;
 import com.uan.bikeshared.interfaces.GetAllUsersResponse;
 import com.uan.bikeshared.interfaces.GetSaldoRequest;
 import com.uan.bikeshared.interfaces.GetSaldoResponse;
+import com.uan.bikeshared.interfaces.LoginRequest;
+import com.uan.bikeshared.interfaces.LoginResponse;
+import com.uan.bikeshared.interfaces.SendPointRequest;
 import com.uan.bikeshared.interfaces.ServiceStatus;
 import com.uan.bikeshared.interfaces.UserXSD;
+import com.uan.bikeshared.interfaces.VerifyUserRequest;
+import com.uan.bikeshared.interfaces.VerifyUserResponse;
+import com.uan.bikeshared.exceptions.AuthenticationException;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +41,10 @@ public class UserEndpoint {
 
         UserModel userModel = new UserModel();
         BeanUtils.copyProperties(request.getUserInfo(), userModel);
+        
+        // Hash the password before saving
+        userModel.setPassword(userService.hashPassword(userModel.getPassword()));
+        
         userService.AddUser(userModel);
         serviceStatus.setStatus("SUCCESS");
         serviceStatus.setMensagem("Adicionado com sucesso!");
@@ -71,6 +81,73 @@ public class UserEndpoint {
         } else {
             response.setSaldo(0);
         }
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "loginRequest")
+    @ResponsePayload
+    public LoginResponse login(@RequestPayload LoginRequest request) {
+        LoginResponse response = new LoginResponse();
+        try {
+            UserModel user = userService.loginUser(request.getTelephone(), request.getPassword(), request.getWifiCodig());
+            
+            if (user != null) {
+                response.setName(user.getName());
+                response.setEmail(user.getEmail());
+                response.setTelephone(user.getTelephone());
+                response.setSaldo(user.getSaldo());
+                response.setId(user.getId());
+                response.setToken(userService.generateToken(user));
+                response.setType(user.getType());
+            } else {
+                throw new AuthenticationException("Credenciais inválidas");
+            }
+        } catch (AuthenticationException e) {
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.setStatus("FAILURE");
+            serviceStatus.setMensagem(e.getMessage());
+            response.setServiceStatus(serviceStatus);
+        }
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "sendPointRequest")
+    @ResponsePayload
+    public ServiceStatus sendPoint(@RequestPayload SendPointRequest request) {
+        ServiceStatus serviceStatus = new ServiceStatus();
+
+        try {
+            boolean success = userService.sendPoints(
+                request.getUserFromId(),
+                request.getWifiCodigUserReceiver(),
+                request.getSaldo(),
+                request.getUserName()
+            );
+
+            if (success) {
+                serviceStatus.setStatus("SUCCESS");
+                serviceStatus.setMensagem("Pontos enviados com sucesso");
+            } else {
+                serviceStatus.setStatus("FAILURE");
+                serviceStatus.setMensagem("Falha ao enviar pontos");
+            }
+        } catch (RuntimeException e) {
+            serviceStatus.setStatus("FAILURE");
+            serviceStatus.setMensagem(e.getMessage());
+        }
+
+        return serviceStatus;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "verifyUserRequest")
+    @ResponsePayload
+    public VerifyUserResponse verifyUser(@RequestPayload VerifyUserRequest request) {
+        VerifyUserResponse response = new VerifyUserResponse();
+        
+        boolean exists = userService.verifyUser(request.getWifiCodig());
+        response.setExist(exists);
 
         return response;
     }
